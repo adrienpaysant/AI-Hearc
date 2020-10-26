40433823d5d9ab340819a2b522059440b934172b
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-10-26 10:40:24 +0100

    new file to be ordered + working on report

---
+++ TP/TP2/2.2_a_Star_villes/aStar.py
@@ -0,0 +1,39 @@
+from queue import PriorityQueue
+from CityLink import *
+
+#heuristiques 
+def h0(n,B): return 0
+def h1(n,B): return B.X-n.X
+def h2(n,B): return B.Y-n.Y
+def h3(n,B): return math.sqrt((B.X-n.X)**2+(B.Y+n.Y)**2)
+def h4(n,B): return h1(n,B)+h2(n,B)
+
+listHeuris=[h0,h1,h2,h3,h4]
+dicHeur={"0":"0","1":"distance selon X","2":"distance selon Y","3":"distance à vol d'oiseau","4":"distance de Manhattan"}
+
+#A*
+def aStar(dicCity,cityA,cityB,h):
+    """ A* search between two city
+        from dicCity with a definned heuristic
+    """
+    frontier = PriorityQueue()
+    frontier.put((0,cityA))
+    sumWeight = {cityA: 0}
+    steps = 0
+
+    while not frontier.empty():
+        temp = frontier.get()
+        steps+=1
+        current=temp[1]
+        if current==cityB:
+            return current, steps
+            
+        for coLeaf in current.getcoLeafs(dicCity):
+            tempWeight = sumWeight[current] + current.getWeightOf(coLeaf)
+            if coLeaf not in sumWeight or tempWeight < sumWeight[coLeaf]:
+                sumWeight[coLeaf] = tempWeight
+                coLeaf.parent = current
+                priority = h(coLeaf, cityB) + tempWeight
+                frontier.put((priority,coLeaf))
+
+    raise Exception("no solution")
