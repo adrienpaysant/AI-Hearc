5425ce18699e3d9c23cd11a72d930c2c42256bb4
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-11-12 12:14:03 +0100

    add folder for TP3 - Genetic Algorithm

---
+++ TP/TP3/labyrinth.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def display_labyrinth(grid, start_cell, end_cell, solution=None):
+    """Display the labyrinth matrix and possibly the solution with matplotlib.
+    Free cell will be in light gray.
+    Wall cells will be in dark gray.
+    Start and end cells will be in dark blue.
+    Path cells (start, end excluded) will be in light blue.
+    :param grid np.array: labyrinth matrix
+    :param start_cell: tuple of i, j indices for the start cell
+    :param end_cell: tuple of i, j indices for the end cell
+    :param solution: list of successive tuple i, j indices who forms the path
+    """
+    grid = np.array(grid, copy=True)
+    FREE_CELL = 19
+    WALL_CELL = 16
+    START = 0
+    END = 0
+    PATH = 2
+    grid[grid == 0] = FREE_CELL
+    grid[grid == 1] = WALL_CELL
+    grid[start_cell] = START
+    grid[end_cell] = END
+    if solution:
+        solution = solution[1:-1]
+        for cell in solution:
+            grid[cell] = PATH
+    else:
+        print("No solution has been found")
+    plt.matshow(grid, cmap="tab20c")
+    
+def solve_labyrinth(grid, start_cell, end_cell, max_time_s):
+    """Attempt to solve the labyrinth by returning the best path found
+    :param grid np.array: numpy 2d array
+    :start_cell tuple: tuple of i, j indices for the start cell
+    :end_cell tuple: tuple of i, j indices for the end cell
+    :max_time_s float: maximum time for running the algorithm
+    :return list: list of successive tuple i, j indices who forms the path
+    """
+    # TODO Implementation..
+    return None
